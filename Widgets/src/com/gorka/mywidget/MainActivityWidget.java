8a80e6c9e4eea5298ff0af10b7ced08839879375
gorkaeff <gorkajm85@gmail.com>
2013-06-07 05:36:48 -0700

    Primer ejemplo Widgets

---
+++ Widgets/src/com/gorka/mywidget/MainActivityWidget.java
@@ -0,0 +1,12 @@
+package com.gorka.mywidget;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+public class MainActivityWidget extends AppWidgetProvider {
+
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+	// actualizar el widget....
+    }
+}
